4a5ef5e5b4092c458b277305e99f907bdfb44472
k1chen <chenaun@bc.edu>
2025-01-20 13:18:42 -0500

    Initial Commit

---
+++ What is Football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  What is Football
+//
+//  Created by Kenneth Chen on 1/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ What is Football/What_is_FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_is_FootballApp.swift
+//  What is Football
+//
+//  Created by Kenneth Chen on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct What_is_FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
